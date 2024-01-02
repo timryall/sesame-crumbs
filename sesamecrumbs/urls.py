8dd68a94b458622c201f16707119ccc8d46c2b21
timryall <88timryall@gmail.com>
2024-01-02 11:01:11 +1000

    feat: working item list

--- sesamecrumbs/urls.py
+++ sesamecrumbs/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # Auth
+    path('', include('core.urls', namespace='core'))
 ]
 
 if settings.DEBUG:
